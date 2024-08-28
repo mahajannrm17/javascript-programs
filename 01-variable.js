3850f0131117ff634b149b76e94d267d0147063c
Niranjani Mahajan <mahajan.niranjani@gmail.com>
2024-08-28 18:59:03 +0530

    javascript-variables
    javascript-variables

---
+++ 01-variable.js
@@ -0,0 +1,15 @@
+// Variable Declaration
+var firstName;
+console.log(firstName);
+
+// Variable Initialisation
+firstName= "Jenny";
+console.log(firstName);
+
+// variable Declaration and Initialisation
+var lastName="Gates";
+console.log(lastName);
+
+// Variable Update
+var firstName="Jennifer";
+console.log(firstName);
