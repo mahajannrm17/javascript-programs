1f1f7df47ff93cbe23326918b71750c98ba2860d
Niranjani Mahajan <mahajan.niranjani@gmail.com>
2024-08-29 01:37:33 +0530

    funAssign01.js
    function Assignment

---
+++ funAssign01.js
@@ -0,0 +1,23 @@
+//1
+function sum(){
+    console.log("Function Without Arguments and Return type");
+}
+console.log("Funtion Sum() looks like this--->",sum());
+//2
+function personalDetails(firstName,lastName,collegeName){
+    console.log("FirstName= ",firstName);
+    console.log("LastName= ",lastName);
+    console.log("collegeName= ",collegeName);
+}
+personalDetails("Jenny","Gates","DEMO");
+
+//3
+function addThreeValues(param1,param2,param3){
+    var result = param1+param2+param3;
+    console.log(result);
+}
+addThreeValues(10.23,600,40);
+addThreeValues("Hello"," Good"," Morning");
+
+
+
