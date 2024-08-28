28b0d4b4c3fd5b2fb43be0844093e59a5a1221a7
Niranjani Mahajan <mahajan.niranjani@gmail.com>
2024-08-29 01:35:53 +0530

    variableAssign.js
    variable assignment

---
+++ 01-variableAssign.js
@@ -0,0 +1,36 @@
+// Assigning Variables. 
+//1.
+console.log("*****1*****");
+var myFullName;
+console.log(myFullName);
+myFullName= "Allen";
+console.log(myFullName);
+myFullName="Allen Alex Aurthur";
+console.log(myFullName);
+console.log("******2*******");
+//2.
+var myLove = "I Love only JS";
+console.log(myLove);
+console.log("******3*****");
+//3
+var firstName="Abc";
+console.log("First Name:",firstName);
+var lastName="XYZ";
+console.log("Last Name:", lastName);
+var age=30;
+console.log("Age:",age);
+var address= "65, Downtown,Georgia";
+console.log("Address:",address);
+var pincode = 0o20265;
+console.log("Pincode:",pincode);
+gender= "M";
+console.log("Gender:",gender);
+isMarried= "yes";
+console.log("Married?:",isMarried);
+
+
+
+
+
+
+
