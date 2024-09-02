9898b05ae4632b12ea57f855db81c075993f51d6
Niranjani Mahajan <mahajan.niranjani@gmail.com>
2024-09-02 16:14:35 +0530

    03-stringBasicAssig01.js
    practice on Strings

---
+++ 03-stringBasicAssig01.js
@@ -0,0 +1,10 @@
+function stringBasic(){
+    console.log("MY DREAM COMPANY:  NPCI");
+}
+stringBasic();
+let str1= "Your hobby1";
+let str2= "Your hobby2";
+let str3= "Your hobby3";
+console.log("Hobbies==> "+str1,str2,str3);
+const totalChar = str1.length+str2.length+str3.length;
+console.log("Total Number of characters in Hobbies 1,2,3 respectively : "+totalChar);
